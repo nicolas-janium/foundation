7431241a88a80a5ea242500563d212fe3ddf3c9a
nicolas <narnold113@gmail.com>
2021-02-14 12:34:24 -0700

    add to return string and modify deploy scripts

--- janium_deploy.sh
+++ janium_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-declare -a deploy_files=( $(git diff --name-only HEAD HEAD~1 | sed 's/main.py/deploy.sh/g') )
+declare -a deploy_files=( $(git diff --name-only main main~1 | sed 's/main.py/deploy.sh/g') )
 
 for i in "${deploy_files[@]}"
 do
