71f31ec96a660e3c684dc58b9d987b8388171d01
nicolas <narnold113@gmail.com>
2021-02-14 16:31:31 -0700

    testing deploy build file

--- janium_deploy.sh
+++ janium_deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-declare -a deploy_files=( $(git diff --name-only HEAD^ HEAD | sed 's/main.py/deploy.sh/g') )
+ls -la
+# declare -a deploy_files=( $(git diff --name-only HEAD^ HEAD | sed 's/main.py/deploy.sh/g') )
 
-for i in "${deploy_files[@]}"
-do
-    if [[ "$i" == *"deploy.sh"* ]]; then
-        bash "$i"
-    fi
-done
+# for i in "${deploy_files[@]}"
+# do
+#     if [[ "$i" == *"deploy.sh"* ]]; then
+#         bash "$i"
+#     fi
+# done
