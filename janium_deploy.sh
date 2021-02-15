ae0eba07610a4109eb6eb65f44251fe47585e1c3
nicolas <narnold113@gmail.com>
2021-02-14 17:15:45 -0700

    modify hello world function

--- janium_deploy.sh
+++ janium_deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-ls -la
-# declare -a deploy_files=( $(git diff --name-only HEAD^ HEAD | sed 's/main.py/deploy.sh/g') )
+declare -a deploy_files=( $(git diff --name-only HEAD^ HEAD | sed 's/main.py/deploy.sh/g') )
 
-# for i in "${deploy_files[@]}"
-# do
-#     if [[ "$i" == *"deploy.sh"* ]]; then
-#         bash "$i"
-#     fi
-# done
+for i in "${deploy_files[@]}"
+do
+    if [[ "$i" == *"deploy.sh"* ]]; then
+        bash "$i"
+    fi
+done
