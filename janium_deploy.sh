18b9113329983d304b1f08b5789acf7c5e5f8906
nicolas <narnold113@gmail.com>
2021-02-14 17:31:14 -0700

    janium_deploy file

--- janium_deploy.sh
+++ janium_deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # ls -la
-git diff --name-only master master~1
-# declare -a deploy_files=( $(git diff --name-only main main~1 | sed 's/main.py/deploy.sh/g') )
+# git diff --name-only master master~1
+declare -a deploy_files=( $(git diff --name-only master master~1 | sed 's/main.py/deploy.sh/g') )
 
-# for i in "${deploy_files[@]}"
-# do
-#     if [[ "$i" == *"deploy.sh"* ]]; then
-#         bash "$i"
-#     fi
-# done
+for i in "${deploy_files[@]}"
+do
+    if [[ "$i" == *"deploy.sh"* ]]; then
+        bash "$i"
+    fi
+done
