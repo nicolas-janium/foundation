33626d73eea096f5da4ea7c816fc4cbead67098d
nicolas <narnold113@gmail.com>
2021-02-19 16:40:07 -0700

    deploy straight from build file and use secrets

--- janium_deploy.sh
+++ janium_deploy.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-# # ls -la
-# git diff --name-only master master~1
-# declare -a deploy_files=( $(git diff --name-only master master~1 | sed 's/main.py/deploy.sh/g') )
-
-# # echo $deploy_files
-
-# for i in "${deploy_files[@]}"
-# do
-#     if [[ "$i" == *"deploy.sh"* ]]; then
-#         bash "$i"
-#     fi
-# done
 
 echo "Deploying function-1..."
 gcloud functions deploy hello_world --region=us-central1 --entry-point=main --runtime=python38 --trigger-http --allow-unauthenticated --source=./janium_functions/hello_world
